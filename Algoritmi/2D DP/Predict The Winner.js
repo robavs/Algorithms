aa8e104a7f613c122b30d66745e1fabf54987ce7
Aleksa Robavs <62261768+robavs@users.noreply.github.com>
2023-02-06 19:30:01 +0100

    Update Predict The Winner.js

--- Algoritmi/2D DP/Predict The Winner.js
+++ Algoritmi/2D DP/Predict The Winner.js
@@ -1,25 +1,16 @@
 const PredictTheWinner = nums => {
     const dp = Array(nums.length).fill(0).map(() => Array(nums.length).fill(undefined))
 
-    const dfs = (l, r, move) => {
+    const dfs = (l, r) => {
         if(l == r + 1)
             return 0
         if(dp[l][r] != undefined)
-            return dp[l][r]
-        let res
-        if(move == 0){
-            res = Math.max(
-                nums[l] + dfs(l + 1, r, 1),
-                nums[r] + dfs(l, r - 1, 1)
+            return dp[l][r]        
+
+        return dp[l][r] = Math.max(
+                nums[l] - dfs(l + 1, r),
+                nums[r] - dfs(l, r - 1)
             )
-        }
-        else {
-            res = Math.min(
-                -nums[l] + dfs(l + 1, r, 0),
-                -nums[r] + dfs(l, r - 1, 0)
-            )
-        }
-        return dp[l][r] = res
     }
-    return dfs(0, nums.length - 1, 0) >= 0
+    return dfs(0, nums.length - 1) >= 0
 };
