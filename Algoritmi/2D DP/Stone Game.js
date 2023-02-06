2809f81e6ea830fbadd0c24c0a5ed7bcce39b9df
Aleksa Robavs <62261768+robavs@users.noreply.github.com>
2023-02-06 19:59:34 +0100

    Update Stone Game.js

--- Algoritmi/2D DP/Stone Game.js
+++ Algoritmi/2D DP/Stone Game.js
@@ -1,3 +1,5 @@
+// moze se napisati i kraci algoritam isti kao i za Predict The Winner
+// koji je inace generalni algoritam ali ovaj radi jer imamo paran broj elemenata
 const stoneGame = piles => {
     const n = piles.length
     const dp = Array(n).fill(0).map(() => Array(n).fill(undefined))
@@ -25,3 +27,20 @@ const stoneGame = piles => {
     }
     return dfs(0, 0, n - 1, 0) > 0
 };
+// Ovo je opsti algoritam (MiniMax)
+const StoneGame = nums => {
+    const dp = Array(nums.length).fill(0).map(() => Array(nums.length).fill(undefined))
+
+    const dfs = (l, r) => {
+        if(l == r + 1)
+            return 0
+        if(dp[l][r] != undefined)
+            return dp[l][r]        
+
+        return dp[l][r] = Math.max(
+                nums[l] - dfs(l + 1, r),
+                nums[r] - dfs(l, r - 1)
+            )
+    }
+    return dfs(0, nums.length - 1) >= 0
+}
