cf2f826cb8981db8bf4129d5050e592980f9043b
Aleksa Robavs <62261768+robavs@users.noreply.github.com>
2023-02-13 01:22:13 +0100

    Add files via upload

---
+++ Algoritmi/Data Structures/Trie/Implement Trie Prefix Tree.js
@@ -0,0 +1,45 @@
+class TrieNode{
+    constructor(){
+        this.children = {}
+        this.endOfWord = false
+    }
+}
+class Trie {
+    constructor(){
+        this.root = new TrieNode()
+    }
+}
+
+
+Trie.prototype.insert = function(word) {
+    let current = this.root
+    for(const char of word){
+        if(!current.children[char])
+            current.children[char] = new TrieNode()
+        current = current.children[char]
+    }
+    current.endOfWord = true
+}
+
+Trie.prototype.search = function(word) {
+    let current = this.root
+
+    for(const char of word){
+        if(!current.children[char])
+            return false
+        current = current.children[char]
+    } 
+    return current.endOfWord
+}
+
+
+Trie.prototype.startsWith = function(prefix) {
+    let current = this.root
+    
+    for(const char of prefix){
+        if(!current.children[char])
+            return false
+        current = current.children[char]
+    }
+    return true
+}
