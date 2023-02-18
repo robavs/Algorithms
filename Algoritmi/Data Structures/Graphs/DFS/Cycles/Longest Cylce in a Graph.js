2462c39f00e464083def8e328fbfba1a4ab0ce17
Aleksa Robavs <62261768+robavs@users.noreply.github.com>
2023-02-18 16:25:46 +0100

    Add files via upload

---
+++ Algoritmi/Data Structures/Graphs/DFS/Cycles/Longest Cylce in a Graph.js
@@ -0,0 +1,27 @@
+const longestCycle = edges => {
+    let max = -1
+    const dp = Array(edges.length).fill(undefined)
+
+    const dfs = (node, dest, depth, visited) => {
+        if (node == -1 || dp[node] != undefined)
+            return -1
+        if (depth > 0 && node == dest)
+            return depth
+        if (!visited.has(edges[node])) {
+            visited.add(edges[node])
+            return dfs(edges[node], dest, depth + 1, visited)
+        }
+        return -1
+    }
+    
+    for (let i = 0; i < edges.length; i++) {
+        const visited = new Set()
+        const res = dfs(i, i, 0, visited)
+        max = Math.max(max, res)
+        if(res > -1)
+            for(let node of visited)
+                dp[node] = 1
+        dp[i] = 1
+    }
+    return max
+};
