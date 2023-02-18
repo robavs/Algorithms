416ec02fa899635b1baa62debfb367286a475d6e
Aleksa Robavs <62261768+robavs@users.noreply.github.com>
2023-02-18 10:48:17 +0100

    Create Number od Enclaves.js

---
+++ Algoritmi/Data Structures/Graphs/DFS/Number od Enclaves.js
@@ -0,0 +1,25 @@
+const numEnclaves = grid => {
+    const m = grid.length
+    const n = grid[0].length
+
+    const isValid = (i, j) => i >= 0 && i < m && j >= 0 && j < n
+
+    const dfs = (i, j) => {
+        if(isValid(i, j) && grid[i][j]){
+            grid[i][j] = 0
+            return 1 + dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i, j - 1)
+        }
+        return 0
+    }
+    let res = 0
+    for(let i = 0; i < m; i++)
+        for(let j = 0; j < n; j++)
+            if(grid[i][j] && (i * j == 0 || i == m - 1 || j == n - 1))
+                dfs(i, j)
+
+    for(let i = 0; i < m; i++)
+        for(let j = 0; j < n; j++)
+            if(grid[i][j])
+                res += dfs(i, j)            
+    return res
+};
