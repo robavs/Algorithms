0b2934f82157772813fc9194e77bc22dba4689e6
Aleksa Robavs <62261768+robavs@users.noreply.github.com>
2023-02-25 14:47:28 +0100

    Update DeleteAndEarn.js

--- Algoritmi/1D DP/DeleteAndEarn.js
+++ Algoritmi/1D DP/DeleteAndEarn.js
@@ -1,5 +1,5 @@
-//zadatak je bas lak ali nisam se setio ideje 
-//da sve iste vrendosti saberes u jedno polje
+// zadatak je bas lak ali nisam se setio ideje 
+// da sve iste vrendosti saberes u jedno polje
 // i da posle radis House robber
 const deleteAndEarn = nums => {
     const arr = Array(10001).fill(0)
@@ -7,6 +7,6 @@ const deleteAndEarn = nums => {
         arr[nums[i]] += nums[i]
     
     for(let i = 2; i < arr.length; i++)
-        arr[i] += Math.max(arr[i-2], arr[i - 3] || 0)
+        arr[i] += Math.max(arr[i - 2], arr[i - 3] || 0)
     return Math.max(arr[10000], arr[9999])
 }
