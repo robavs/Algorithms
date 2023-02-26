edbc60683f14122b21d6770dd068e06d6337d94b
Aleksa Robavs <62261768+robavs@users.noreply.github.com>
2023-02-26 23:38:07 +0100

    Create Course Schedule.js

---
+++ Algoritmi/Data Structures/Graphs/DFS/Cycles/Course Schedule.js
@@ -0,0 +1,37 @@
+// cylce detection bez topological sort
+const canFinish = (n, edges) => {
+    const graph = Array(n).fill(0).map(() => [])
+    const dp = Array(n).fill(0)
+
+    for (let i = 0; i < edges.length; i++) {
+        const a = edges[i][0]
+        const b = edges[i][1]
+        if (a == b) return false
+        graph[a].push(b)
+    }
+
+    const dfs = (node, visited) => {
+        if (dp[node] == 1)
+            return false
+
+        if (visited.has(node))
+            return true
+
+        visited.add(node)
+
+        for (let i = 0; i < graph[node].length; i++) 
+            if (dfs(graph[node][i], visited))
+                return true
+    
+        visited.delete(node)
+        return false
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (dfs(i, new Set()))
+            return false
+        dp[i] = 1
+    }
+
+    return true
+};
