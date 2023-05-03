686124ad762e3513be1d94206c16c24a606a1270
Aleksa Robavs <62261768+robavs@users.noreply.github.com>
2023-05-03 13:53:05 +0200

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import './assets/css/style.css'
+import React from 'react'
+import General from './components/General/General';
+
+const App = () => {
+  return (
+    <>
+      <General />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/General/General.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Clients from './Clients';
+import Stats from './Stats';
+import Services from './Services';
+import Testimonials from './Testimonials';
+import Team from './Team';
+import Pricing from './Pricing';
+import FAQ from './FAQ';
+import Contact from './Contact';
+import Footer from './Footer';
+import Loading from "../Loading";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
+const General = () => {
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        setIsLoading(false)
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        })
+    }, [])
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    return (
+        <>
+            <Header />
+            <Home />
+            <About />
+            <Clients />
+            <Stats />
+            <Services />
+            <Testimonials />
+            <Team />
+            <Pricing />
+            <FAQ />
+            <Contact />
+            <Footer />
+        </>
+    )
+}
+
+export default General
